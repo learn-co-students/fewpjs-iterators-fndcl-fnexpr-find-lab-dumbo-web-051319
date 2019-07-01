70b120d0374a3a4cd99eac827841697e021fbd96
sticklercarla <carlastickler@gmail.com>
2019-07-01 10:14:44 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array) {
+  
+  let obj = array.find(o => (o.result === "W"))
+    if (obj === undefined) {
+      return undefined 
+    } else {
+      return obj.year
+    }
+}
