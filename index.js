d545a49d6f3542383cc0eb54b1f404f041a05c9a
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-07-07 17:24:21 -0400

    used find iterator

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const win = array.find(a => a.result === "W");
+  if(win) {
+    return win.year
+  }
+}
