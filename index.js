12d1c2bfa068e5ee6c5eff88f036d9528c6544af
Stefanos <stefanosu422@gmail.com>
2019-07-13 20:45:33 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const record = [
+  { year:'2018' , result:'L'},
+  { year:'1969', result: "W" },
+  { year: '2016', result: 'N/A'}
+]
+
+ const superbowlWin = (record) => { 
+ let result = record.find( record => record.result === "W")
+  return !!result ? result.year : undefined
+}
+   
