0a9a4cc499114511ff683d7d7a254a807d86e916
Tess Neau <tessneau@gmail.com>
2019-11-11 22:07:15 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  const win = record.find(object => object.result === "W")
+  if (win) {
+    return win.year
+  }
+  return win
+}
